87860d92183d8a470d2a96acad5884f2e914ed4b
Noah Pixler <noahpixler03@gmail.com>
2025-11-10 12:23:50 -0600

    Added Delete Functionality to application

--- app/page.tsx
+++ app/page.tsx
@@ -31,14 +31,22 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        {todos.map((todo) => <li 
+        
+        onClick={() => deleteTodo(todo.id)}
+        key={todo.id}>
+        {todo.content}
+        
+        </li>)}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
