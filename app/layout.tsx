bce0f146038064a7b60a64cab42328bb766abc33
Noah Pixler <noahpixler03@gmail.com>
2025-11-10 13:31:12 -0600

    Added Authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
@@ -17,6 +18,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
+*/
+import React from "react";
+import { Amplify } from "aws-amplify";
+import "./app.css";
+import AuthenticatorWrapper from "./AuthenticatorWrapper";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "@/amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>      
+        <AuthenticatorWrapper>
+          {children}
+        </AuthenticatorWrapper>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -14,6 +15,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -55,6 +57,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
