1305e9a7b472135b91c0c50cb343d1ccd7c8d007
Noah Pixler <noahpixler03@gmail.com>
2025-12-01 10:47:46 -0600

    Undoing re-implementation of Authenticator

--- app/components/ToDo.tsx
+++ app/components/ToDo.tsx
@@ -5,7 +5,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "../../app/app.css";
 import { Amplify } from "aws-amplify";
-import { useAuthenticator } from "@aws-amplify/ui-react";
+// import { useAuthenticator } from "@aws-amplify/ui-react";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -15,7 +15,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { user, signOut } = useAuthenticator();
+  // const { user, signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
